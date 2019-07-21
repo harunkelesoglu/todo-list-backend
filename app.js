b15ae618cd1205611954fd7f3803392e54bf7060
harunkelesoglu <harunkelesoglu_@hotmail.com>
2019-07-22 00:59:27 +0300

    fixed welcome page and response type

--- app.js
+++ app.js
@@ -5,20 +5,27 @@ const ToDoController = require('./controllers/todos');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const response = {};
+let response = {};
 
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-  res.send('client/build/index.html');
+  response = {
+    statusCode: 200,
+    status: true,
+    message: 'Welcome to app',
+  };
+  res.status(response.statusCode).json(response);
 });
 app.use('/api/v1', ToDoController);
 app.use((err, req, res) => {
-  response.statusCode = 500;
-  response.status = false;
-  response.message = err.message;
+  response = {
+    statusCode: 500,
+    status: false,
+    message: err.message,
+  };
   res.status(response.statusCode).json(response);
 });
 
